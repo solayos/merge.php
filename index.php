b233227e6cacc945a1dc31e4c55530b53ebecf7d
Brendo Marinho <brendonmm@outlook.com>
2013-10-25 15:29:44 -0700

    Inicialização do projeto

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>MergeSortAPP</title>
+    <meta name="description" content="Algorithm to merge sorting values of a array.">
+    <meta name="keywords" content="javascript, merge, mergesort">
+	<meta charset="UTF-8" />
+	
+	<link href="/css/style.css" rel="stylesheet" type="text/css" />
+    
+	<script src="/js/jquery-2.0.3.min.js" type="text/javascript"></script>
+    <!--<script src="/js/jquery-1.10.2.min.js" type="text/javascript"></script>-->
+	
+    <script src="/js/script.js" type="text/javascript"></script>
+	
+</head>
+<body>
+    <div class="main">
+        <header>
+            <hgroup>
+                <h1 class="bold">
+                    MergeSortAPP
+                </h1>
+            </hgroup>
+        </header>
+        <div class="content">
+			<div id="areaScript">
+				<div id="botaoSortear">Sortear</div>
+			</div>
+        </div>
+    </div>
+    <footer>
+		<hgroup>
+			<h2>
+				@Plaft
+			</h2>
+		</hgroup>
+		<p>Implementação do Algotitmo MergeSort em JavaScript</p>
+    </footer>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+    $('#areaScript ul li').hide();
+    $('#areaScript #botaoSortear').click(function () {
+        var i = 0;
+        var quantidade = parseInt(prompt("Quantos numeros vamos ordenar? \t\n (com ate 3 digitos)"));
+        var areaScript = 'areaScript';
+        var base = 'base';
+
+        //criaElementos (pai, tipo, quantidade)
+        var tipo = base;
+        $('#' + areaScript).criaFilho(areaScript, tipo, 1);
+        
+        if (quantidade > 8) {
+            $('#' + areaScript + ' .base').css('height', '9em');
+        }
+
+        i = 0;
+        while (i < quantidade) {
+            var listaLocal = base;
+            $('#' + areaScript + ' .' + tipo).criaLista(listaLocal, quantidade);
+
+
+            $('#' + areaScript + ' .' + listaLocal + ' ul li').eq(i).show().animate(
+                {
+                    opacity: 1,
+                    color: "#000000",
+                    padding: "5px 5px",
+                    width: "60px"
+                }, "slow");
+            i++;
+        }
+    });
+});
+
+$.fn.criaFilho = function (pai, tipo, quantidade) {
+    meuPai = $('#' + pai);
+    if (tipo === 'base' || tipo === 'result' || tipo === 'childlevel1' || tipo === 'childlevel2' || tipo === 'childlevel3') {
+        var filho = $('<div>').attr('id', tipo);
+        meuPai.append(filho);
+        meuPai.find('.' + tipo).hide();
+    }
+};
+
+$.fn.criaLista = function (pai, limite) {
+    meuPai = $('#' + pai);
+    var i = 0;
+    var lista = meuPai.append('<ul>');
+    alert(limite);
+    while (i < limite) {
+        var item = lista.append('<li>');
+        var itemList = getNumbers();
+        var valor = item.text(itemList);
+        item.eq(i).append(valor);
+        i++;
+    }
+};
+
+function removeItem(item) {
+    var itemRemover = item.lastChild;
+    removeChild(itemRemover);
+}
+
+function getNumbers() {
+    var entr;
+    do {
+        entr = prompt("Digite um numero");
+    } while (entr == null || entr == "");
+    return entr;
+}
+
+function merge_sort(arr) {
+    var l = arr.length, m = Math.floor(l / 2);
+    if (l <= 1) return arr;
+    return merge(merge_sort(arr.slice(0, m)), merge_sort(arr.slice(m)));
+}
+
+function merge(left, right) {
+    var result = [];
+    var ll = left.length, rl = right.length;
+    while (ll > 0 && rl > 0) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+            ll--;
+        } else {
+            result.push(right.shift());
+            rl--;
+        }
+    }
+    if (ll > 0) {
+        result.push.apply(result, left);
+    } else if (rl > 0) {
+        result.push.apply(result, right);
+    }
+    return result;
+}
+
+/* codes in javascript */
+function criaFilhoJS(pai, tipo, quantidade) {
+    if (tipo === 'base' || tipo === 'result' || tipo === 'childlevel1' || tipo === 'childlevel2' || tipo === 'childlevel3') {
+        var minhaBase = document.getElementById(pai);
+        console.log(minhaBase);
+        var filho = minhaBase.innerHTML();
+        minhaBase.setAttribute('class', tipo);
+    }
+}
+
+function criaListaJS(pai, limite) {
+    var i = 0;
+    var lista = pai.createElement('ul');
+
+    while (i < limite) {
+        var itemList = getNumbers();
+        var item = lista.createElement('li');
+        var valor = document.createTextNode(itemList);
+        item.eq(i).appendChild(valor);
+        i++;
+    }
+}
